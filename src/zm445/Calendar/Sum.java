28f766a8709c6fdcb5e9de974b23eea9dd564b62
DESKTOP-KJNJGQL\kim hyun jung <dlwnsgur303@naver.com>
2021-11-22 14:59:31 +0900

    두 수를 입력받아 합 출력하기(내가 만듬) 저번 시간에 배운 코드를 보고 그것과 유사하게 만듬 문자열로 입력받은 후 배열에 쪼개 넣고 숫자로 바꾼 후 더해 출력함

---
+++ src/zm445/Calendar/Sum.java
@@ -0,0 +1,22 @@
+package zm445.Calendar;
+
+import java.util.Scanner;
+
+public class Sum {
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("두 수를 입력하세요.");
+		String inputValue = scan.nextLine();
+		
+		String[] splitedValue = inputValue.split(" ");
+		
+		int first = Integer.parseInt(splitedValue[0]);
+		int second = Integer.parseInt(splitedValue[1]);
+		int total = first + second;
+		
+		System.out.println("두 수의 합은 " + total + "입니다.");
+		
+	}
+} 
