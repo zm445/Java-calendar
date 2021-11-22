4b56ceea24482154cb905fffab6616ec8e191423
DESKTOP-KJNJGQL\kim hyun jung <dlwnsgur303@naver.com>
2021-11-22 16:04:41 +0900

    숫자를 입력받아 해당하는 달의 최대일수를 출력하는 프로그램 만듬

--- src/zm445/Calendar/calendar.java
+++ src/zm445/Calendar/calendar.java
@@ -1,5 +1,7 @@
 package zm445.Calendar;
 
+import java.util.Scanner;
+
 public class calendar {
 
 	public static void main(String[] args) {
@@ -11,6 +13,23 @@ public class calendar {
 		System.out.println(" 8  9 10 11 12 13 14");
 		System.out.println("15 16 17 18 19 20 21");
 		System.out.println("22 23 24 25 26 27 28");
+		
+		// 숫자를 입력받아 해당하는 달의 최대일수를 출력하는 프로그램
+		
+		Scanner scan = new Scanner(System.in);
+		
+		int[] result = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+		
+		System.out.println("달을 입력하세요.");
+		int month = scan.nextInt();
+		scan.close();
+		
+		if(month < 1 || 12 < month) {
+			System.out.printf("잘못 입력하셨습니다.");
+			return;
+		}
+		System.out.printf("%d월은 %d일까지 있습니다. \n", month, result[month-1]);
+		
 		
 	}
 }
