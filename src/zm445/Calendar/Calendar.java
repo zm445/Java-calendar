2932691b1a6ac3b1b8dba7e5a7c2cbe9f8a2686a
DESKTOP-KJNJGQL\kim hyun jung <dlwnsgur303@naver.com>
2021-11-23 11:53:40 +0900

    Update Calendar

--- src/zm445/Calendar/Calendar.java
+++ src/zm445/Calendar/Calendar.java
@@ -8,29 +8,28 @@ import java.util.Scanner;
 //-1달을 입력받을 경우 프로그램을 종료하도록 구현
 
 public class Calendar {	
-	private static final int[] MAX_DAYS = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+	private static int[] MAY_DAYS = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};	
 	
-	public int getMaxOfDay(int month) {
-		return MAX_DAYS[month - 1];
+	public int getMaxOfDays(int month) {
+		return MAY_DAYS[month - 1];
 	}	
-	public static void main(String[] args) {
+	public static void main(String[] args) {		
 		String PROMPT = "cal> ";
 		Scanner scanner = new Scanner(System.in);
 		Calendar cal = new Calendar();
-				
 		while(true) {
-			System.out.printf("달을 입력하세요: ");
+			System.out.println("월을 입력하세요.");
 			System.out.print(PROMPT);
 			int month = scanner.nextInt();
 			if(month == -1) {
 				break;
 			} else if(month < 1 || month > 12) {
-				System.out.println("잘못 입력하셨습니다");
+				System.out.println("잘못 입력하셨습니다.");
 				continue;
 			}
-			System.out.printf("%d월은 %d일까지 있습니다. \n", month, cal.getMaxOfDay(month));
-		}	
-		System.out.println("Have a nice day");
+			System.out.printf("%d월은 %d일까지 있습니다. \n", month, cal.getMaxOfDays(month));
+		}
+		System.out.println("good Bye~");
 		scanner.close();
 	}
 }
