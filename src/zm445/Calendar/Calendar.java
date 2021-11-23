d0ad8709ad2e99fdfa909e34dd0636ad118e7929
DESKTOP-KJNJGQL\kim hyun jung <dlwnsgur303@naver.com>
2021-11-23 11:27:22 +0900

    숫자를 입력받아 해당하는 달의 최대일수를 출력하는 프로그램 반복입력을 받을 수 있고 입력하는 곳 앞에 프롬프트를 출력, -1달을 입력받을 경우 프로그램을 종료하도록 구현

--- src/zm445/Calendar/Calendar.java
+++ src/zm445/Calendar/Calendar.java
@@ -2,8 +2,10 @@ package zm445.Calendar;
 
 import java.util.Scanner;
 
-//먼저 반복할 횟수를 입력받은 후 그 횟수만큼 
-//숫자를 입력받아 해당하는 달의 최대일수를 출력하는 프로그램
+//숫자를 입력받아 해당하는 달의 최대일수를 출력하는 프로그램 
+//반복입력을 받을 수 있고
+//입력하는 곳 앞에 프롬프트를 출력,
+//-1달을 입력받을 경우 프로그램을 종료하도록 구현
 
 public class Calendar {	
 	private static final int[] MAX_DAYS = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
@@ -12,17 +14,23 @@ public class Calendar {
 		return MAX_DAYS[month - 1];
 	}	
 	public static void main(String[] args) {
+		String PROMPT = "cal> ";
 		Scanner scanner = new Scanner(System.in);
 		Calendar cal = new Calendar();
-		System.out.printf("반복할 횟수를 입력하세요: ");
-		int repeat = scanner.nextInt();
-		
-		for(int i=0; i<repeat; i++) {
+				
+		while(true) {
 			System.out.printf("달을 입력하세요: ");
-			int month = scanner.nextInt();			
+			System.out.print(PROMPT);
+			int month = scanner.nextInt();
+			if(month == -1) {
+				break;
+			} else if(month < 1 || month > 12) {
+				System.out.println("잘못 입력하셨습니다");
+				continue;
+			}
 			System.out.printf("%d월은 %d일까지 있습니다. \n", month, cal.getMaxOfDay(month));
 		}	
-		System.out.println("Bye~");
+		System.out.println("Have a nice day");
 		scanner.close();
 	}
 }
